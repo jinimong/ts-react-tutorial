48053d90c380e90ca15a4bc187b405e7c6611c2e
jwkim <phoongdeng@gmail.com>
2021-03-03 02:46:14 +0900

    Add reducer example

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,9 +1,28 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+
+type Action =
+  | {
+      type: 'INCREASE';
+    }
+  | {
+      type: 'DECREASE';
+    };
+
+function reducer(state: number, action: Action): number {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+    case 'DECREASE':
+      return state - 1;
+    default:
+      throw new Error('Unhandled action');
+  }
+}
 
 const Counter = () => {
-  const [count, setCount] = useState<number>(0);
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
+  const [count, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: 'INCREASE' });
+  const onDecrease = () => dispatch({ type: 'DECREASE' });
   return (
     <div>
       <h1>{count}</h1>
@@ -14,3 +33,5 @@ const Counter = () => {
     </div>
   );
 };
+
+export default Counter;
